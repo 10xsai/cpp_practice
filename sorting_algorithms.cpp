b95fb1f09c01b7ae09745acb303bd93ba38db11c
krishnagottipalli <saikrishna.gdsv@gmail.com>
2021-02-28 14:23:22 +0530

    added selection_sort bubble_sort

---
+++ sorting_algorithms.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+void selection_sort(int *arr, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min_index = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_index])
+            {
+                min_index = j;
+            }
+        }
+        swap(arr[i], arr[min_index]);
+    }
+}
+
+void bubble_sort(int *arr, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        bool flag = false;
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                flag = true;
+            }
+        }
+        if (!flag)
+            break;
+    }
+}
